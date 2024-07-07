9968fedd33e4e0bd14a7f169f9085ca597b4923f
movva-gpu <danyellastrikann@duck.com>
2024-07-08 00:43:03 +0200

    feat: :sparkles: initial commit
    adds `.vscode` config
    adds `go.mod` module
    adds `.gitignore` template
    adds `main.go` added
    learning basics of go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module works.danyella/go-mplayer-mod
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+const MY_FIRST_CONST string = "Hello"
+
+const PATCH_VERSION uint8 = 1
+const MINOR_VERSION uint8 = 0
+const MAJOR_VERSION uint8 = 0
+
+const APP_NAME string = "Go Music Player TUI"
+const APP_NAME_SHORT string = "gmp"
+
+const APP_LICENSE string = "MIT"
+const APP_AUTHOR string = "Danyella Strikann"
+const APP_URL string = "https://github.com/danyell/go-mplayer-tui"
+
+var APP_VERSION string = fmt.Sprintf("%d.%d.%d", MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
+
+func main() {
+
+	myFirstColonVar := "Loafs"
+
+	fmt.Println(MY_FIRST_CONST+",", myFirstColonVar+"!")
+
+	fmt.Println()
+	fmt.Println(APP_NAME_SHORT+",", "v"+APP_VERSION)
+
+}
